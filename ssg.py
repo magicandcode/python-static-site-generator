52316b549aaa84af04c95ed06106a7d22ddf73c7
magicandcode <magicandcode@gmail.com>
2021-04-08 17:07:01 +0200

    Completed module 1

---
+++ ssg.py
@@ -0,0 +1,17 @@
+import typer
+
+from ssg.site import Site
+
+
+def main(source='content', dest='dist'):
+    config = {
+        'source': source,
+        'dest':
+        dest
+    }
+
+    Site(**config).build()
+
+
+if __name__ == '__main__':
+    typer.run(main)
